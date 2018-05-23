b3417fe78acedbeef3e9a3aa63339e680feece38
Elliot Briant <elliotbriant@Elliots-MacBook-Pro.local>
2018-05-23 09:01:57 -0700

    simple contract

---
+++ simple_contract.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.4;
+
+//When you compile a contract the instance of that contract gets stores on
+//the enthereum blockchain so unlike other programming languages the instance
+//is stored in memory all the values is stored in the blockchain
+
+contract myFirstContract {
+  string private name;
+  uint private age;
+
+  function setName(string newName) {
+    name = newName;
+  }
+
+  function getName() return (string) {
+    return name;
+  }
+
+}
